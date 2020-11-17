45a4e9547c93d51812cc3040862450a24e8d3d3a
visceroi@gmail.com <visceroi@gmail.com>
2020-11-18 00:37:40 +0300

    removing old files

--- fiera.node.js
+++
@@ -1,8 +0,0 @@
-global.DEVELOPMENT = false;
-global.fabric = require("./../plugins/fabric.1.6.4").fabric;
-global.caman = require("Caman");
-global._ = require("underscore");
-
-require("./fiera.js");
-global.fabric.debug = false;
-module.exports = global.fabric;
